4511f602377b281c8a39ef59fd3fa33658de2320
Haydar Evren <haydaremin@gmail.com>
2021-11-18 10:39:34 -0500

    Update strokes_to_paint.py

--- strokes_to_paint/strokes_to_paint.py
+++ strokes_to_paint/strokes_to_paint.py
@@ -7,15 +7,16 @@ class Solution:
         count = 0
         directions=[(-1,0),(0,1),(1,0),(0,-1)]
         def findIsland(x,y):
+            color = grid[x][y]
             for dx, dy in directions:
                 nx,ny = x+dx, y+dy
-                if 0<=nx<row and 0<=ny<col and grid[nx][ny]=='1' and (nx,ny) not in visited:
+                if 0<=nx<row and 0<=ny<col and grid[nx][ny]== color and (nx,ny) not in visited:
                     visited.add((nx,ny))
                     findIsland(nx,ny)
             
         for x in range(row):
             for y in range(col):
-                if grid[x][y] == '1' and (x,y) not in visited:
+                if (x,y) not in visited:
                     count +=1
                     visited.add((x,y))
                     findIsland(x,y)         
