e6d4ffc9ad81d171052d3a077c1e7a12f12ae686
Haydar Evren <haydaremin@gmail.com>
2021-12-03 09:38:27 -0500

    Create dungeon-game.py
    Dynamic Programming, Complexity: O(m.n)

---
+++ dungeon-game.py
@@ -0,0 +1,20 @@
+# 174. Dungeon Game
+# https://leetcode.com/problems/dungeon-game/
+
+class Solution:
+    def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
+        
+        m = len(dungeon)
+        n = len(dungeon[0])
+        
+        healthreq = [[10001 for j in range(n+1)] for i in range(m+1)]
+        
+        for i in range(m-1,-1,-1):
+            for j in range(n-1,-1,-1):
+                if i==m-1 and j==n-1:
+                    healthreq[i][j]= max(1,1-dungeon[i][j])
+                else:
+                    bestroute=min(healthreq[i+1][j]- dungeon[i][j], healthreq[i][j+1]- dungeon[i][j]) #up vs left
+                    healthreq[i][j]= max(1, bestroute ) 
+
+        return healthreq[0][0]
