0bcf494b4c8098e296b6b872709ba64f74f3da3a
Haydar Evren <haydaremin@gmail.com>
2021-12-14 14:01:22 -0500

    Priority Queues, Heap
    Time Complexity: O(NlogN)

---
+++ meeting-rooms-ii.py
@@ -0,0 +1,21 @@
+# 253. Meeting Rooms II
+# https://leetcode.com/problems/meeting-rooms-ii/
+
+import heapq
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        if len(intervals) == 1: return 1
+        
+        intervals.sort()
+        p_que = [intervals[0][1]]
+        rooms = 1
+
+        for s,e in intervals[1:]:
+            if s < p_que[0]:
+                heapq.heappush(p_que,e)
+                rooms +=1
+            else:
+                heapq.heapreplace(p_que,e)
+                
+        return rooms
